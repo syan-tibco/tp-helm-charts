f352482a48650af1b5893194787952dc6792a144
dmiller via svc-msgbuild <dmiller@tibco.com>
2023-11-01 21:22:21 +0000

    publish new chart: msg-ems-tp-1.0.19

--- charts/msg-ems-tp/scripts/mk-output.conf.sh
+++ charts/msg-ems-tp/scripts/mk-output.conf.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-# Copyright (c) 2023 Cloud Software Group, Inc. All Rights Reserved. Confidential and Proprietary.
+#
+# Copyright (c) 2023. Cloud Software Group, Inc.
+# This file is subject to the license terms contained 
+# in the license file that is distributed with this file.  
+#
 
 outfile=${1:-output.conf}
 cat - <<EOF > $outfile
