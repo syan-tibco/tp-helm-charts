d129a87e6104618d66144e1c39340d6a5b53fab8
Nachiket Ashtaputre <nashtapu@tibco.com>
2023-11-03 00:34:08 +0530

    PDP-1334: further changes

--- dp-cluster-workshop/aks/aks-cluster-create.sh
+++ dp-cluster-workshop/aks/aks-cluster-create.sh
@@ -1,24 +1,74 @@
-export DP_RESOURCE_GROUP=dp-resource-group
-export DP_CLUSTER_NAME=dp-cluster
-export AZURE_REGION=eastus
-export AUTHORIZED_IP="103.243.236.10"
-export VNET_CIDR="10.224.0.0/12"
+export SUBSCRIPTION_ID="14669e18-e772-4744-bf93-72f677ccb9aa"
+export TENANT_ID="cde6fa59-abb3-4971-be01-2443c417cbda"
+export DP_RESOURCE_GROUP="dp-resource-group"
+export DP_CLUSTER_NAME="dp-cluster"
+export AZURE_REGION="eastus"
+export USER_ASSIGNED_IDENTITY_NAME="dp-cluster-identity"
+export AUTHORIZED_IP="103.243.236.10/32,58.84.62.35/32"
+export VNET_NAME="dp-cluster-vnet"
+export VNET_CIDR="10.4.0.0/16"
+export AKS_SUBNET_NAME="aks-subnet"
+export AKS_SUBNET_CIDR="10.4.0.0/20"
+export APPLICATION_GW_SUBNET_NAME="appgw-subnet"
+export APPLICATION_GW_SUBNET_CIDR="10.4.17.0/24"
+export NAT_GW_NAME="nat-gateway"
+export NAT_GW_SUBNET_NAME="natgw-subnet"
+export NAT_GW_SUBNET_CIDR="10.4.18.0/27"
+export PUBLIC_IP_NAME="public-ip"
 
 # create resource group
 az group create --location "${AZURE_REGION}" --name "${DP_RESOURCE_GROUP}"
 
-echo "start to create AKS: ${DP_RESOURCE_GROUP}/${DP_CLUSTER_NAME}"
+# create user-assigned identity
+az identity create --name "${USER_ASSIGNED_IDENTITY_NAME}" --resource-group "${DP_RESOURCE_GROUP}"
+export USER_ASSIGNED_ID="/subscriptions/${SUBSCRIPTION_ID}/resourcegroups/${DP_RESOURCE_GROUP}/providers/Microsoft.ManagedIdentity/userAssignedIdentities/${USER_ASSIGNED_IDENTITY_NAME}"
+export USER_ASSIGNED_CLIENT_ID="$(az identity show --resource-group "${DP_RESOURCE_GROUP}" --name "${USER_ASSIGNED_IDENTITY_NAME}" --query 'clientId' -otsv)"
+export USER_ASSIGNED_PRINCIPAL_ID="$(az identity show --resource-group "${DP_RESOURCE_GROUP}" --name "${USER_ASSIGNED_IDENTITY_NAME}" --query 'principalId' -otsv)"
+export USER_ASSIGNED_PRINCIPAL_ID="$(az identity show --resource-group "${DP_RESOURCE_GROUP}" --name "${USER_ASSIGNED_IDENTITY_NAME}" --query 'principalId' -otsv)"
+
+# create public ip
+az network public-ip create -g "${DP_RESOURCE_GROUP}" -n ${PUBLIC_IP_NAME} --sku "Standard" --allocation-method "Static"
+export PUBLIC_IP_ID="/subscriptions/${SUBSCRIPTION_ID}/resourceGroups/${DP_RESOURCE_GROUP}/providers/Microsoft.Network/publicIPAddresses/${PUBLIC_IP_NAME}"
+
+# create nat gateway
+az network nat gateway create --resource-group "${DP_RESOURCE_GROUP}" --name "${NAT_GATEWAY_NAME}" --public-ip-prefixes --public-ip-addresses "${PUBLIC_IP_ID}"
+export NAT_GATEWAY_ID="/subscriptions/${SUBSCRIPTION_ID}/resourceGroups/${DP_RESOURCE_GROUP}/providers/Microsoft.Network/natGateways/${NAT_GATEWAY_NAME}"
+
+# append public ip
+export PUBLIC_IP=$(az network public-ip show -g ${DP_RESOURCE_GROUP} -n ${PUBLIC_IP_NAME}  --query 'ipAddress' -otsv)
+export AUTHORIZED_IP="103.243.236.10/32,58.84.62.35/32,${PUBLIC_IP}"
+
+# create virtual network
+az network vnet create -g "${DP_RESOURCE_GROUP}" -n "${VNET_NAME}" --address-prefix "${VNET_CIDR}" 
+
+# create application gateway subnets
+az network vnet subnet create -g ${DP_RESOURCE_GROUP} --vnet-name "${VNET_NAME}" -n "${APPLICATION_GW_SUBNET_NAME}" --address-prefixes "${APPLICATION_GW_SUBNET_CIDR}"
+export APPLICATION_GW_SUBNET_ID="/subscriptions/${SUBSCRIPTION_ID}/resourceGroups/${DP_RESOURCE_GROUP}/providers/Microsoft.Network/virtualNetworks/${VNET_NAME}/subnets/${APPLICATION_GW_SUBNET_NAME}"
+
+# create aks subnet
+az network vnet subnet create -g ${DP_RESOURCE_GROUP} --vnet-name "${VNET_NAME}" -n "${AKS_SUBNET_NAME}" --address-prefixes "${AKS_SUBNET_CIDR}" --nat-gateway "${NAT_GATEWAY_ID}"
+export AKS_VNET_SUBNET_ID="/subscriptions/${SUBSCRIPTION_ID}/resourceGroups/${DP_RESOURCE_GROUP}/providers/Microsoft.Network/virtualNetworks/${VNET_NAME}/subnets/${AKS_SUBNET_NAME}"
+
+# create nat gateway subnet
+az network vnet subnet create -g ${DP_RESOURCE_GROUP} --vnet-name "${VNET_NAME}" -n "${NAT_GW_SUBNET_NAME}" --address-prefixes "${NAT_GW_SUBNET_CIDR}" --nat-gateway "${NAT_GATEWAY_ID}"
+export NAT_GW_VNET_SUBNET_ID="/subscriptions/${SUBSCRIPTION_ID}/resourceGroups/${DP_RESOURCE_GROUP}/providers/Microsoft.Network/virtualNetworks/${VNET_NAME}/subnets/${NAT_GW_SUBNET_NAME}"
+
 # create aks cluster
+echo "start to create AKS: ${DP_RESOURCE_GROUP}/${DP_CLUSTER_NAME}"
 az aks create -g "${DP_RESOURCE_GROUP}" -n "${DP_CLUSTER_NAME}" \
-  --enable-managed-identity \
-  --node-count 2 \
-  --enable-addons monitoring \
-  --enable-msi-auth-for-monitoring \
+  --node-count 3 \
+  --enable-addons ingress-appgw \
+  --enable-msi-auth-for-monitoring false \
   --generate-ssh-keys \
   --api-server-authorized-ip-ranges "${AUTHORIZED_IP}" \
   --enable-oidc-issuer \
   --enable-workload-identity \
   --network-plugin azure \
   --kubernetes-version "1.28.0" \
-  --outbound-type managedNATGateway
+  --outbound-type userAssignedNATGateway \
+  --appgw-name gateway \
+  --vnet-subnet-id "${AKS_VNET_SUBNET_ID}" \
+  --appgw-subnet-id "${APPLICATION_GW_SUBNET_ID}" \
+  --assign-identity "${USER_ASSIGNED_ID}" \
+  --assign-kubelet-identity "${USER_ASSIGNED_ID}"
 echo "finished creating AKS"
